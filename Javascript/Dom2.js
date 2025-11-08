7cac15b8dc22e931cb6f193b6c698080875c22af
AnjaliKachhi <anjalikachhi69972@gmail.com>
2025-11-08 22:20:44 +0530

    dom

---
+++ Javascript/Dom2.js
@@ -0,0 +1,32 @@
+// let div=document.querySelector("div");
+// console.log(div);
+// let id=div.getAttribute("id");
+// console.log(id);
+// let name=div.getAttribute("name");
+// console.log(name);
+// let para=document.querySelector("p");
+// console.log(para.getAttribute("class"));
+// console.log(para.setAttribute("class","123"));
+
+let div=document.querySelector("div");
+console.log("div");
+div.style.backgroundColor="green";
+// div.style.visibility="hidden";
+div.style.fontSize="27px";
+
+// let newbtn=document.createElement("button");
+// newbtn.innerText="click me";
+// console.log(newbtn);
+
+// let divs =document.querySelector("div");
+// for adding in the last
+// divs.append(newbtn);
+// for adding inthe front
+// divs.prepend(newbtn);
+// for just before it
+// divs.before(newbtn);
+// for after the node
+// 
+let newheading =document.createElement("h1");
+newheading.innerHTMl="<i> HI, i am new here</i>";
+document.querySelector("body").prepend(newheading);
