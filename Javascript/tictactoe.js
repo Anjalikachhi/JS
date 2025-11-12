8401cc86713bb0a20ffa7b3455409f806c984c43
AnjaliKachhi <anjalikachhi69972@gmail.com>
2025-11-12 10:44:36 +0530

    tic tac toe

--- Javascript/tictactoe.js
+++ Javascript/tictactoe.js
@@ -3,7 +3,8 @@ let resetbtn=document.querySelector("#reset-btn");
 let newGamebtn=document.querySelector("#new-btn");
 let msgcontainer=document.querySelector(".msg-container");
 let msg=document.querySelector("#msg");
-
+let draw=document.querySelector("#draw-msg");
+let count=0;
 
 let turn0=true;
 const winPatterns=[
@@ -12,29 +13,42 @@ const winPatterns=[
 
 const resetgame=()=>{
     turn0=true;
+    count=0;
     enableBoxes();
     msgcontainer.classList.add("hide");
+    draw.innerText="";
+    msg.innerText="";
 
 };
 
-
-
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
-console.log("box was clicked");
 if(turn0===true){
     box.innerText="0";
+    box.classList.add("o");
     turn0=false;
 
 }else{
     box.innerText="X";
+    box.classList.add("x");
     turn0=true;
 }
 box.disabled=true;
+count++;
 
-checkWinner();
+const isWinner=    checkWinner();
+if( !isWinner&&count===9 ){
+    showDraw();
+}
     });
 });
+const showDraw=()=>{
+    draw.innerText="its a draw";
+    msg.innerText="";
+    msgcontainer.classList.remove("hide");
+    disableBoxes();
+
+};
 
 const disableBoxes= ()=>{
     boxes.forEach((box) => {
@@ -49,6 +63,7 @@ box.innerText="";
   });}
 const showWinner = (winner) =>{
     msg.innerText=`Congratulations ,winner is ${winner}`;
+    draw.innerText="";
     msgcontainer.classList.remove("hide");
     disableBoxes();
 };
@@ -62,11 +77,12 @@ const showWinner = (winner) =>{
         if(Posi1 ===Posi2&& Posi2===Posi3){
             console.log("winner");
             showWinner(Posi1);
+            return true;
         }
     }
-};
+}return false;
 
-}
+};
 
 newGamebtn.addEventListener("click",resetgame);
 resetbtn.addEventListener("click",resetgame);
