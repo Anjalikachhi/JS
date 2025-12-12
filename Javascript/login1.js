7d87079342ac7ac2365a66fdae76759a4078e609
AnjaliKachhi <anjalikachhi69972@gmail.com>
2025-12-12 23:55:19 +0530

    login page simple

---
+++ Javascript/login1.js
@@ -0,0 +1,12 @@
+function login() {
+    let user = document.getElementById("username").value;
+    let pass = document.getElementById("password").value;
+
+    if (user === "admin" && pass === "1234") {
+        document.getElementById("message").style.color = "green";
+        document.getElementById("message").innerText = "Login Successful!";
+    } else {
+        document.getElementById("message").style.color = "red";
+        document.getElementById("message").innerText = "Invalid username or password!";
+    }
+}
